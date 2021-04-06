7e8164259760f45de126796e09f7001fe24239fe
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-04-06 16:57:37 -0500

    add hooks to the counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 
 import Counter from './Counter'
+import CounterHooks from './CounterHooks';
 
 function App() {
   return (
+    <div>
+    Counter
    <Counter initialCount={0}/>
+   Counter Hooks
+   <CounterHooks initialCount={0}/>
+   </div>
   );
 }
 
